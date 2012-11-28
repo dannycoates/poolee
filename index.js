594ce6ce6e23d04c204c53c1defcbcbeaae5aca2
Danny Coates <dannycoates@gmail.com>
2012-11-27 16:08:23 -0800

    rewrote the ping logic to fix several ping related issues
    The previous code would ping too frequently and could get stuck
    in a ping loop, even if the endpoint was actually healthy.
    
    The new code factors out the ping logic into its own module. The
    logic for setting the endpoint health has be constrained to exactly
    two locations, one for setting as healthy and one for unhealthy.
    
    Pinger ensures that only one ping request is pending at a given time
    and that the ping loop is independent of other timeouts. The pinger
    uses exponential backoff for timing ping requests with a maximum
    delay of 10s.
    
    The tests were updated for the new behavior.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inherits = require('util').inherits,
     EventEmitter = require('events').EventEmitter,
-    Endpoint = require('./lib/endpoint')(inherits, EventEmitter),
+    Pinger = require('./lib/pinger')(inherits, EventEmitter),
+    Endpoint = require('./lib/endpoint')(inherits, EventEmitter, Pinger),
     RequestSet = require('./lib/request_set')
 
 module.exports = require('./lib/pool')(inherits, EventEmitter, Endpoint, RequestSet)
